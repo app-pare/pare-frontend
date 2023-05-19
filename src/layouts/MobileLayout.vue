8260439ab986f206312da45e02961dd096848bfa
Darkson Bezerra <darkson@MacBook-Air-de-Darkson.local>
2023-05-18 21:10:59 -0300

    ajuste no menu

--- src/layouts/MobileLayout.vue
+++ src/layouts/MobileLayout.vue
@@ -1,21 +1,19 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-page-container>
-      <!-- Menu móvel -->
-      <q-toolbar class="mobile-menu" flat>
-        <div class="q-gutter-md justify-center">
-          <q-btn round dense flat icon="home" v-on:click="goToHome" />
-          <q-btn round dense flat icon="class" v-on:click="goToClasses" />
-          <q-btn round dense flat icon="add" v-on:click="goToAdd" />
-          <q-btn round dense flat icon="favorite" v-on:click="goToFavorites" />
-          <q-btn round dense flat icon="search" v-on:click="goToSearch" />
-        </div>
-      </q-toolbar>
-
-      <!-- Conteúdo da página -->
       <div class="router-view-container">
         <router-view />
       </div>
+      <!-- Menu móvel -->
+      <q-toolbar class="mobile-menu" flat>
+        <div class="q-gutter-md justify-center">
+          <q-btn round dense flat icon="home" @click="goToHome" />
+          <q-btn round dense flat icon="class" @click="goToClasses" />
+          <q-btn round dense flat icon="add" @click="goToAdd" />
+          <q-btn round dense flat icon="favorite" @click="goToFavorites" />
+          <q-btn round dense flat icon="search" @click="goToSearch" />
+        </div>
+      </q-toolbar>
     </q-page-container>
   </q-layout>
 </template>
@@ -66,7 +64,7 @@ export default defineComponent({
 }
 
 .router-view-container {
-  margin-bottom: 48px; /* Espaçamento para evitar que o conteúdo fique oculto pelo menu */
+  margin-bottom: 56px;
 }
 
 .q-btn--flat:focus,
