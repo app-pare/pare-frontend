275d5815bfdf6ea2e3db9c87540009333a2f036f
Nikely Fedechen <nikelyfedechen@Nikelys-MacBook-Air.local>
2023-05-20 16:45:50 -0300

    ajuste de cores da pag de login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,35 +1,39 @@
 <template>
-  <q-page class="page-centered">
-    <div class="q-pa-md">
-      <div class="q-gutter-md">
-        <div class="text-h6">
-          Bem-vindo(a) ao PARE, faça login para continuar.
+  <q-page class="full-height">
+    <div class="row justify-center align-center">
+      <div class="col-10 col-xs-4 col-sm-4 col-md-3">
+        <div>
+          <h1>
+            Bem-vindo(a) ao PARE, <br />
+            faça login para continuar.
+          </h1>
+          <p class="subtitle">
+            Ainda não tem conta? <a>Cadastre-se aqui</a>. <br />
+            Leva menos de um minuto.
+          </p>
         </div>
-        <div class="text-subtitle2">
-          Ainda não tem conta? <u>Cadastre-se aqui</u>. Leva menos de um minuto
+        <div>
+          <q-form @submit="submitForm">
+            <label>E-mail<span>*</span></label>
+            <q-input
+              rounded
+              outlined
+              v-model="email"
+              label="Digite seu e-mail"
+              type="email"
+            />
+            <label class="input-header">Senha<span>*</span></label>
+            <q-input
+              rounded
+              outlined
+              v-model="password"
+              label="Digite sua senha"
+              type="password"
+            />
+            <q-btn label="Login" type="submit" class="btn full-width" />
+            <div class="text-center"><a>Esqueceu sua senha?</a></div>
+          </q-form>
         </div>
-      </div>
-      <div class="q-gutter-md">
-        <q-form @submit="submitForm">
-          <div class="input-header">E-mail*</div>
-          <q-input
-            rounded
-            outlined
-            v-model="email"
-            label="Digite seu e-mail"
-            type="email"
-          />
-          <div class="input-header">Senha*</div>
-          <q-input
-            rounded
-            outlined
-            v-model="password"
-            label="Digite sua senha"
-            type="password"
-          />
-          <q-btn label="Login" type="submit" class="btn-submit" />
-          <div class="q-mt-md"><a>Esqueceu sua senha?</a></div>
-        </q-form>
       </div>
     </div>
   </q-page>
@@ -53,68 +57,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style scoped>
-.page-centered {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-}
-.text-h6 {
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 700;
-  font-size: 22px;
-  line-height: 130%;
-  /* or 23px */
-  color: #000000;
-  padding-inline-end: 30px;
-}
-
-.text-subtitle2 {
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 600;
-  font-size: 12px;
-  line-height: 130%;
-  /* or 16px */
-  color: #000000;
-  padding-inline-end: 70px;
-}
-
-.input-header {
-  margin-top: 25px;
-  font-family: "Montserrat";
-  font-style: bold;
-  font-weight: 700;
-  font-size: 14px;
-  line-height: 17px;
-  /* identical to box height */
-  color: #000000;
-  margin-bottom: 5px;
-}
-
-.q-pa-md {
-  padding: 1rem;
-  margin-top: 86px;
-}
-
-.q-gutter-md > * {
-  margin-bottom: 38px;
-}
-
-.btn-submit {
-  margin-top: 25px;
-  background-color: black;
-  color: white;
-  border-radius: 15px;
-  width: 100%;
-  padding: 16px 120px;
-}
-.q-mt-md {
-  margin-top: 1rem;
-  width: 100%;
-  text-align: center;
-}
-</style>
