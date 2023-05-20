d3dc9ce19c8ee7fe137ca1c83d15cf241c8a6395
Nikely Fedechen <nikelyfedechen@Nikelys-MacBook-Air.local>
2023-05-20 14:55:43 -0300

    Feature/estilizacao da pagina home

--- src/pages/HomeUserPage.vue
+++ src/pages/HomeUserPage.vue
@@ -1,71 +1,25 @@
 <template>
-  <q-page class="page-centered">
-    <div class="q-pa-md">
-      <div class="q-gutter-md">
-        <q-row align="center">
-          <q-col cols="auto" class="user-info">
-            <span class="user-name">Olá, Fulana!</span>
-            <q-icon name="person" class="user-icon" />
-          </q-col>
-          <q-col cols="auto">
-            <button>Criar nova coleção</button>
-          </q-col>
-        </q-row>
-        <div class="home-buttons">
-          <div class="button-item">
-            <button>Meus Cards</button>
-          </div>
-          <div class="button-item">
-            <button>Estudar</button>
-          </div>
-          <div class="button-item">
-            <button>Revisar</button>
-          </div>
-          <div class="button-item">
-            <button>Convidar amigos</button>
-          </div>
+  <q-page class="full-height">
+    <div>
+      <div class="row justify-center">
+        <div class="col-10 col-xs-5 col-sm-5 col-md-4">
+          <q-btn icon="add" class="btn full-width" label="Criar uma coleção" />
+          <q-btn class="my-colections full-width" label="Minhas coleções" />
+          <q-btn class="my-flashcads full-width" label="Meus flashcards" />
+          <q-card class="my-card">
+            <p class="text-h6">Estudar</p>
+            <img src="../assets/Dictionary-pana.svg" />
+          </q-card>
+          <q-card class="my-card">
+            <p class="text-h6">Revisar</p>
+            <img src="../assets/Mobile-note-list-cuate.svg" />
+          </q-card>
+          <q-card class="my-card">
+            <p class="text-h6">Convidar amigos</p>
+            <img src="../assets/Online-world-cuate.svg" />
+          </q-card>
         </div>
       </div>
     </div>
   </q-page>
 </template>
-
-<style scoped>
-.page-centered {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-}
-
-.q-gutter-md > * {
-  margin-bottom: 1rem;
-}
-
-.user-info {
-  display: flex;
-  align-items: center;
-}
-
-.user-name {
-  margin-right: 0.5rem;
-}
-
-.user-icon {
-  margin-left: 0.5rem;
-}
-
-.home-buttons {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.button-item {
-  margin-bottom: 1rem;
-}
-
-.q-mt-md {
-  margin-top: 1rem;
-}
-</style>
