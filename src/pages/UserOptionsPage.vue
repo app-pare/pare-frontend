c83332c57a8778ff62745eae0e16e294bf6f7669
Nikely Fedechen <nikelyfedechen@Nikelys-MacBook-Air.local>
2023-05-21 12:11:43 -0300

    Adicionando metodo click no botao

--- src/pages/UserOptionsPage.vue
+++ src/pages/UserOptionsPage.vue
@@ -3,8 +3,18 @@
     <div>
       <div class="row justify-center">
         <div class="col-10 col-xs-5 col-sm-5 col-md-4">
-          <q-btn icon="add" class="btn full-width" label="Criar uma coleção" />
-          <q-btn icon="add" class="btn full-width" label="Criar um flashcard" />
+          <q-btn
+            @click="createCollection"
+            icon="add"
+            class="btn full-width"
+            label="Criar uma coleção"
+          />
+          <q-btn
+            @click="createFlashCard"
+            icon="add"
+            class="btn full-width"
+            label="Criar um flashcard"
+          />
         </div>
       </div>
     </div>
