b494292a26dd371f81f7013dcb63178b4eff9c9f
Nikely Fedechen <nikelyfedechen@Nikelys-MacBook-Air.local>
2023-05-22 21:49:36 -0300

    Ajuste de padrão

--- src/pages/CreateNewCollectionPage.vue
+++ src/pages/CreateNewCollectionPage.vue
@@ -1,89 +1,91 @@
 <template>
   <q-page class="full-height">
     <div class="row justify-center">
-      <q-form @submit="submitForm">
-        <div class="q-gutter-md">
-          <div class="input-header">Nome da coleção</div>
-          <q-input
-            rounded
-            outlined
-            label="Nome da coleção"
-            v-model="nomeColecao"
-            autofocus
-          ></q-input>
-          <div class="input-header">Incluir flashcards</div>
-          <q-select
-            rounded
-            outlined
-            label="Incluir flashcards"
-            v-model="incluirFlashcards"
-          >
-            <q-option label="Opção 1" value="opcao1"></q-option>
-            <q-option label="Opção 2" value="opcao2"></q-option>
-          </q-select>
-          <div class="input-header">Data do exame (opcional)</div>
-          <q-input
-            rounded
-            outlined
-            type="date"
-            label="Data do exame (opcional)"
-            v-model="dataExame"
-          ></q-input>
-          <div class="input-header">Cor</div>
-          <div class="color-select">
-            <q-radio
-              keep-color
-              v-model="corSelecionada"
-              val="red"
-              color="red"
-            ></q-radio>
-            <q-radio
-              keep-color
-              v-model="corSelecionada"
-              val="pink"
-              color="pink"
-            ></q-radio>
-            <q-radio
-              keep-color
-              v-model="corSelecionada"
-              val="blue"
-              color="blue"
-            ></q-radio>
-            <q-radio
-              keep-color
-              v-model="corSelecionada"
-              val="purple"
-              color="purple"
-            ></q-radio>
-            <q-radio
-              keep-color
-              v-model="corSelecionada"
-              val="amber"
-              color="amber"
-            ></q-radio>
-            <q-radio
-              keep-color
-              v-model="corSelecionada"
-              val="cyan"
-              color="cyan"
-            ></q-radio>
-            <q-radio
-              keep-color
-              v-model="corSelecionada"
-              val="teal"
-              color="teal"
-            ></q-radio>
-          </div>
+      <div class="col-10 col-xs-4 col-sm-4 col-md-3">
+        <q-form @submit="submitForm">
           <div>
-            <q-btn
-              type="submit"
-              color="primary"
-              label="Cadastrar coleção"
-              class="btn full-width"
-            />
+            <label>Nome da coleção</label>
+            <q-input
+              rounded
+              outlined
+              label="Nome da coleção"
+              v-model="nomeColecao"
+              autofocus
+            ></q-input>
+            <label>Incluir flashcards</label>
+            <q-select
+              rounded
+              outlined
+              label="Incluir flashcards"
+              v-model="incluirFlashcards"
+            >
+              <q-option label="Opção 1" value="opcao1"></q-option>
+              <q-option label="Opção 2" value="opcao2"></q-option>
+            </q-select>
+            <label>Data do exame (opcional)</label>
+            <q-input
+              rounded
+              outlined
+              type="date"
+              label="Data do exame (opcional)"
+              v-model="dataExame"
+            ></q-input>
+            <label>Cor</label>
+            <div class="color-select">
+              <q-radio
+                keep-color
+                v-model="corSelecionada"
+                val="red"
+                color="red"
+              ></q-radio>
+              <q-radio
+                keep-color
+                v-model="corSelecionada"
+                val="pink"
+                color="pink"
+              ></q-radio>
+              <q-radio
+                keep-color
+                v-model="corSelecionada"
+                val="blue"
+                color="blue"
+              ></q-radio>
+              <q-radio
+                keep-color
+                v-model="corSelecionada"
+                val="purple"
+                color="purple"
+              ></q-radio>
+              <q-radio
+                keep-color
+                v-model="corSelecionada"
+                val="amber"
+                color="amber"
+              ></q-radio>
+              <q-radio
+                keep-color
+                v-model="corSelecionada"
+                val="cyan"
+                color="cyan"
+              ></q-radio>
+              <q-radio
+                keep-color
+                v-model="corSelecionada"
+                val="teal"
+                color="teal"
+              ></q-radio>
+            </div>
+            <div>
+              <q-btn
+                type="submit"
+                color="primary"
+                label="Cadastrar coleção"
+                class="btn full-width"
+              />
+            </div>
           </div>
-        </div>
-      </q-form>
+        </q-form>
+      </div>
     </div>
   </q-page>
 </template>
@@ -105,49 +107,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.page-centered {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-}
-
-.text-h6 {
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 17px;
-  /* identical to box height */
-
-  color: #000000;
-}
-.input-header {
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 600;
-  font-size: 14px;
-  line-height: 17px;
-  /* identical to box height */
-
-  color: #000000;
-}
-
-.color-select {
-  justify-content: space-around;
-}
-
-.btn-submit {
-  margin-top: 25px;
-  background-color: #505050;
-  color: white;
-  border-radius: 15px;
-  padding: 16px 75px;
-}
-
-.q-gutter-md {
-  margin-top: 1rem;
-}
-</style>
